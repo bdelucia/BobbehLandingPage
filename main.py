a02047c51daa00d393aaa4b3521f815326cf13bb
Robert (Bobby) DeLucia <72625310+bdelucia@users.noreply.github.com>
2024-10-02 22:55:13 -0700

    Maybe will work

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 app.register_blueprint(views, url_prefix="/")
 
-if __name__ == '__main__':
-    app.run(debug=True, port=8000)
+##if __name__ == '__main__':
+    ##app.run(debug=True, port=8000)
 
 
