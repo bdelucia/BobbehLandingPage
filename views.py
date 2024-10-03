05601a414899c9521be1e8df5903f0ab5e67c11f
Robert DeLucia Jr. <bdelucia@asu.edu>
2024-10-03 01:03:23 -0700

    weather api cleaning up

--- views.py
+++ views.py
@@ -1,5 +1,7 @@
 import os
 import openai
+import requests
+import math
 from dotenv import load_dotenv
 from flask import Blueprint, render_template
 
@@ -10,6 +12,12 @@ views = Blueprint(__name__, "views")
 load_dotenv()
 openai_api_key = os.getenv("OPENAI_API_KEY")
 openai.api_key = openai_api_key
+openweather_api_key = os.getenv("OPENWEATHER_API_KEY")
+# Function to generate weather data
+def get_weather_data(city):
+    url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={openweather_api_key}&units=imperial"
+    response = requests.get(url)
+    return response.json()
 
 # Function to generate response from ChatGPT
 def chat_gpt(prompt):
@@ -22,6 +30,6 @@ def chat_gpt(prompt):
 # Renders index.html with passed chatGPT-generated funFact
 @views.route("/")
 def home():
-    load_dotenv()
-    openweather_api_key = os.getenv("OPENWEATHER_API_KEY")
-    return render_template("index.html", funFact = chat_gpt("Tell me a fun fact"), openweather_api_key=openweather_api_key)
+    weather_data = get_weather_data("85308")
+    rounded_temp = math.ceil(weather_data["main"]["temp"])
+    return render_template("index.html", funFact = chat_gpt("Tell me a fun fact"), weather_data=weather_data, rounded_temp=rounded_temp)
